b2197b74f905fb5bb085ddb8e36b9fa552750929
igorssalgado <igorssalgado@outlook.com>
2024-06-22 18:50:48 -0300

    feat: created /order and /order/:numeroPedido routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,13 @@
 import { Router } from "express";
 
+import {
+  createOrder,
+  getOrderByNumeroPedido,
+} from "./controllers/OrderController.js";
+
 const routes = Router();
+
+routes.post("/order", createOrder);
+routes.get("/order/:numeroPedido", getOrderByNumeroPedido);
 
 export default routes;
