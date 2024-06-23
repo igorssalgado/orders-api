01d14c88a72410282ffca15f892fb70ef3c18abe
igorssalgado <igorssalgado@outlook.com>
2024-06-23 07:51:37 -0300

    feat: implemented the service for getOrderByOrderId, to remove any logic in the controller

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -1,7 +1,7 @@
 import Order from "../models/Orders.js";
 import {
   transformOrderData,
-  getOrderIdForDeletion,
+  getOrderId,
   orderIdExists,
 } from "../services/OrderService.js";
 
@@ -26,32 +26,24 @@ const createOrder = async (request, response) => {
 //get an order data by OrderId
 const getOrderByOrderId = async (request, response) => {
   try {
-    //gets the orderId value from the URL param
-    const orderId = request.params.orderId;
+    //check if the orderId exists
+    const idFound = await orderIdExists(request.params.orderId);
 
-    const idFound = await orderIdExists(orderId);
-
+    //if idFound is not true, throw error
     if (idFound != true) {
-      throw new Error("orderId not found.");
+      throw new Error();
     }
 
-    //lookup for the order using the orderId in the database to get all its information
-    const order = await Order.find({ orderId: orderId });
-
-    //retrieve the order _id, from the order JSON returned in the map array in string
-    let retrieveOrderId = order.map((item) => item._id.toString());
+    //gets the orderId value from the URL param e calls getOrderId service, to get order id to findById
+    const id = await getOrderId(request.params.orderId);
 
     //uses the order id in the 0 index of the map's array and find it in the database
-    const orderFound = await Order.findById({ _id: retrieveOrderId[0] });
-
-    console.log(orderFound);
+    const orderFound = await Order.findById({ _id: id });
 
     //returns 200 once the order is found and return it as JSON.
     return response.status(200).json(orderFound);
   } catch (error) {
     return response.json({ response: error.message });
-  } finally {
-    console.log("fim");
   }
 };
 
@@ -72,13 +64,16 @@ const updateOrder = async (request, response) => {};
 //delete order
 const deleteOrder = async (request, response) => {
   try {
-    const idFound = await orderIdExists(orderId);
+    //check if the orderId exists
+    const idFound = await orderIdExists(request.params.orderId);
 
+    //if idFound is not true, throw error
     if (idFound != true) {
       throw new Error();
     }
-    //gets the orderId value from the URL param e calls getOrderIdForDeletion service, to get order id to delete
-    const id = await getOrderIdForDeletion(request.params.orderId);
+
+    //gets the orderId value from the URL param e calls getOrderId service, to get order id to delete
+    const id = await getOrderId(request.params.orderId);
 
     //uses the order id in the 0 index of the map's array and find it in the database
     await Order.findByIdAndDelete({ _id: id });
