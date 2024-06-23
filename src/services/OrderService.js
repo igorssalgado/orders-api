6ed5acc884240d75244a63d5e93793bdc00efacf
igorssalgado <igorssalgado@outlook.com>
2024-06-22 22:02:49 -0300

    feat: created getOrderIdForDeletion service for order delete

--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -1,6 +1,9 @@
 // import { OrderController } from '../controllers/OrderController.js';
+import Order from "../models/Orders.js";
+
 
 const renameKey = (orderNew) => {
+  //get the order JSON, stringify it and replace the keys names to the translated text
   orderNew = JSON.stringify(orderNew).replace("numeroPedido", "orderId");
   orderNew = orderNew.replace("valorTotal", "value");
   orderNew = orderNew.replace("dataCriacao", "creationDate");
@@ -17,4 +20,16 @@ const transformOrderData = async (order) => {
   return orderNew;
 };
 
-export { transformOrderData };
+//transformOrderData
+const getOrderIdForDeletion = async (orderId) => {
+  //lookup for the order using the orderId in the database to get all its information
+  const order = await Order.find({ orderId: orderId });
+
+  //retrieve the order id, from the order JSON returned in the map array
+  let retrieveOrderId = order.map((item) => item._id.toString());
+
+  //returns the order id to be deleted in the database
+  return retrieveOrderId;
+};
+
+export { transformOrderData, getOrderIdForDeletion };
