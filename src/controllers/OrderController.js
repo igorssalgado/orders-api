a1ca60d96b637b581d8eccdd184311c99e36851f
igorssalgado <igorssalgado@outlook.com>
2024-06-22 21:29:22 -0300

    style:  translated NumeroPedido to OrderId and update createOrder to use transformOrderData service.

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -1,32 +1,37 @@
 import Order from "../models/Orders.js";
+import { transformOrderData } from "../services/OrderService.js";
 
 //create new order
 const createOrder = async (request, response) => {
-    const order = request.body;
+  const order = request.body;
 
-    //gets the assincronous response from the Database, of the order's data from the request body using the Order's model.
-    const newOrder = await Order.create(order);
 
-    //returns 201 once the order is created and sent to the Database
-    return response.status(201).json(newOrder);
+  //gets the transformed JSON from the service
+  const newOrder = await transformOrderData(order);
+
+  //gets the asynchronous response from the Database, of the order's data from the request body using the Order's model.
+  const successful = await Order.create(newOrder);
+
+  //returns 201 once the order is created and sent to the Database
+  return response.status(201).json(successful);
 };
 
-//get an order data by numeroPedido
-const getOrderByNumeroPedido = async (request, response) => {
-    //gets the numeroPedido value from the URL param
-    const numeroPedido = request.params.numeroPedido;
+//get an order data by OrderId
+const getOrderByOrderId = async (request, response) => {
+  //gets the orderId value from the URL param
+  const orderId = request.params.orderId;
 
-    //lookup for the order using the numeroPedido in the database to get all its information
-    const order = await Order.find({numeroPedido: numeroPedido});
+  //lookup for the order using the orderId in the database to get all its information
+  const order = await Order.find({ orderId: orderId });
 
-    //retrieve the order _id, from the order JSON returned in the map array in string
-    let retrieveOrderId = order.map((item)=> item._id.toString());
+  //retrieve the order _id, from the order JSON returned in the map array in string
+  let retrieveOrderId = order.map((item)=> item._id.toString());
 
-    //uses the order id in the 0 index of the map's array and find it in the database
-    const orderFound = await Order.findById({_id: retrieveOrderId[0]});
+  //uses the order id in the 0 index of the map's array and find it in the database
+  const orderFound = await Order.findById({_id: retrieveOrderId[0]});
 
-     //returns 200 once the order is found and return it as JSON.
-    return response.status(200).json(orderFound);
+  //returns 200 once the order is found and return it as JSON.
+  return response.status(200).json(orderFound);
 };
 
 //list all orders
@@ -39,7 +44,7 @@ const updateOrder = async (request, response) => {};
 
 //delete order
 const deleteOrder = async (request, response) => {
-   
+
 };
 
-export {createOrder, getOrderByNumeroPedido };
+export {createOrder, getOrderByOrderId};
